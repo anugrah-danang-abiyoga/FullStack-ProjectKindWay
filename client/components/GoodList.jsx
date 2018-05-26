66dfe836356137ac564444938a8c55eaef809829
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-05-26 14:03:46 +1200

    GoodList is on progress

--- client/components/GoodList.jsx
+++ client/components/GoodList.jsx
@@ -1,2 +1,22 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import { addGoodDeed, editGoodDeed, deleteGoodDeed } from '..actions/gooddeed'
+
+class GoodList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            deed: '',
+            showInputField: false
+        }
+    }
+    toggleForm() {
+        this.setState({showInputField: !this.state.showInputField})
+    }
+    handleChange(e) {
+        this.setState({[e.target.deed]: e.target.value})
+    }
+    editDeed(e, edit) {
+        e.preventDefault() 
+    }
+}
